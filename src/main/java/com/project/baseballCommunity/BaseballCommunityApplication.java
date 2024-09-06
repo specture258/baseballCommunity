12f5bef69709ffd27abf7e9dd30b14b4143714ff
specture258 <specture258@hanyang.ac.kr>
2024-09-06 14:18:24 +0900

    Init: First Commit
    jpa 설정, build.gradle 설정

---
+++ src/main/java/com/project/baseballCommunity/BaseballCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.project.baseballCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballCommunityApplication.class, args);
+	}
+
+}
